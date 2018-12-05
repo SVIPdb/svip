cc93924e57020c1ade7b5d9bfd0f15a7455758eb
faisala <alquaddoomi@nexus.ethz.ch>
2018-12-05 09:52:35 +0100

    Added config to allow djnago to detect that it's running behind an https proxy, e.g., nginx.

--- svip_api/svip_server/settings.py
+++ svip_api/svip_server/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'svip-dev.nexus.ethz.ch']
 
+# allows django to detect that we're running behind a secure proxy (e.g., nginx)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # Application definition
 
