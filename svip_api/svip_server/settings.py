62120afc9c824577bdd1c4b4f9264df27f0d2fc7
faisala <alquaddoomi@nexus.ethz.ch>
2019-01-28 14:05:34 +0100

    Changed STATIC_URL to resolve to a directory below django's control

--- svip_api/svip_server/settings.py
+++ svip_api/svip_server/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 
 # django-rest-framework config
 REST_FRAMEWORK = {
