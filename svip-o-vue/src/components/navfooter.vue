958aba0ffc4a14e429f782cc08c4d761721aa252
faisala <alquaddoomi@nexus.ethz.ch>
2019-01-29 19:34:24 +0100

    More file tabulation and some slight visual tweaks (e.g., more padding in the footer items).

--- svip-o-vue/src/components/navfooter.vue
+++ svip-o-vue/src/components/navfooter.vue
@@ -1,14 +1,16 @@
 /* eslint-disable */
 <template>
-	<!-- footer -->
-	<footer class="footer">
-	<div class = 'container-fluid'>
-		<div class = 'row' style = 'width: 100%'>
-			<div class = 'col-4 text-center'><img src="../assets/SIB_logo_20ans.png" alt="SIB Logo 20ans" class = 'footer_logo'></div>
-			<div class = 'col-4 text-center'><img src="../assets/eth_logo.png" alt="ETH"  class = 'footer_logo'></div>
-			<div class = 'col-4 text-center'><img src="../assets/logo-hes-so-noir-433.png" alt="HES-SO"  class = 'footer_logo'></div>
-		</div>
-	</div>
+    <!-- footer -->
+    <footer class="footer">
+        <div class='container-fluid'>
+            <div class='row' style='width: 100%'>
+                <div class='col-4 text-center'><img src="../assets/SIB_logo_20ans.png" alt="SIB Logo 20ans"
+                                                    class='footer_logo'></div>
+                <div class='col-4 text-center'><img src="../assets/eth_logo.png" alt="ETH" class='footer_logo'></div>
+                <div class='col-4 text-center'><img src="../assets/logo-hes-so-noir-433.png" alt="HES-SO"
+                                                    class='footer_logo'></div>
+            </div>
+        </div>
     </footer>
 </template>
 
@@ -17,28 +19,26 @@
 import Vue from 'vue'
 
 export default {
-  name: 'navHeader',
-  computed: {
-    year () {
-      return new Date().getFullYear()
+    name: 'navHeader',
+    computed: {
+        year() {
+            return new Date().getFullYear()
+        }
     }
-  }
 
 }
-
 </script>
 
 <style>
-img.footer_logo{
-	max-height: 100px;
-	width: auto;
+img.footer_logo {
+    max-height: 100px;
+    width: auto;
 }
 
-footer{
-	position: absolute;
-	width: 100%;
-	bottom: 0;
+footer {
+    position: absolute;
+    width: 100%;
+    bottom: 0;
     background-color: #f5f5f5;
 }
 </style>
-/* eslint-disable */
