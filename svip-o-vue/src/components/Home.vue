986c65fee7a7ac3fd82509feee07d82ab5e2a96d
faisala <alquaddoomi@nexus.ethz.ch>
2019-01-29 19:33:35 +0100

    Added a new search bar, added 'SVIP variants only' setting to the store.

--- svip-o-vue/src/components/Home.vue
+++ svip-o-vue/src/components/Home.vue
@@ -1,95 +1,108 @@
 /* eslint-disable */
 <template>
-	<div class = 'container'>
-		<div class="highlight-clean">
-			<div class="container">
-				<div class="intro section text-center">
-					<!-- <h2 class="text-center">SVIP-O</h2>
-					<p class="text-center">Swiss Variant Interpretation Platform for Oncology </p> -->
-					<img src="../assets/svip_logo.png" width="368" height="137" alt="Svip Logo">
-				</div>
-				<div class = 'section row justify-content-md-center'>
-					<div class = 'col-6'>
-						<!-- <form ><input type="search" placeholder="Search for gene / variant" class="form-control" /></form> -->
-						<form>
-							<v-select :options="options" value='' placeholder="Search for gene / variant" v-model="gene"></v-select>
-					</form>
-				</div>
-			</div>
+    <div class='container'>
+        <div class="highlight-clean">
+            <div class="container">
+                <div class="intro section text-center">
+                    <!-- <h2 class="text-center">SVIP-O</h2>
+                              <p class="text-center">Swiss Variant Interpretation Platform for Oncology </p> -->
+                    <img src="../assets/svip_logo.png" width="368" height="137" alt="Svip Logo">
+                </div>
+                <div class='section row justify-content-md-center'>
+                    <div class='col-6'>
+                        <!-- <form ><input type="search" placeholder="Search for gene / variant" class="form-control" /></form> -->
+                        <!--
+                        <form>
+                            <v-select :options="options" value='' placeholder="Search for gene / variant"
+                                      v-model="gene"></v-select>
+                        </form>
+                        -->
 
-			<div class = 'section stats row justify-content-md-center'>
-				<div class = 'col'>
-					<h3>{{nbGenes}} Genes</h3>
-				</div>
-				<div class = 'col'>
-					<h3>{{nbVariants}} Variants</h3>
-				</div>
-				<!-- <div class = 'col'>
-					<h3>{{nbUniquePhenotypes}} Phenotypes</h3>
-				</div> -->
-			</div>
+                        <SearchBar></SearchBar>
+                    </div>
+                </div>
 
-		</div>
-	</div>
-</div>
+                <div class='section stats row justify-content-md-center'>
+                    <div class='col'>
+                        <h3>{{nbGenes}} Genes</h3>
+                    </div>
+                    <div class='col'>
+                        <h3>{{nbVariants}} Variants</h3>
+                    </div>
+                    <!-- <div class = 'col'>
+                                <h3>{{nbUniquePhenotypes}} Phenotypes</h3>
+                            </div> -->
+                </div>
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 
-import { mapGetters } from 'vuex'
+import {mapGetters} from 'vuex'
 import store from '@/store'
 import {serverURL} from '@/app_config'
+import SearchBar from "./widgets/SearchBar";
+
 export default {
-  name: 'home',
-  data () {
-    return {
-      gene: {}
-    }
-  },
-  watch: {
-    gene: function (n, o) {
-      if (n.value) {
-        let geneIdx = _.findIndex(this.genes, g => { return g.entrez_id === n.value })
-        let id = ''
-        if (geneIdx > -1) {
-          let url = this.genes[geneIdx].url
-          id = url.replace(serverURL + 'genes', '')
-          id = id.replace(/\D/, '')
+    name: 'home',
+    components: {SearchBar},
+    data() {
+        return {
+            gene: {}
         }
-        if (id) this.$router.push('gene/' + id)
-      }
-    }
-  },
-  computed: {
-  	  ...mapGetters({
-  	  	  genes: 'genes',
-		  variants: 'variants',
-		  phenotypes: 'phenotypes',
-		  nbVariants: 'nbVariants',
-		  nbPhenotypes: 'nbPhenotypes',
-		  nbGenes: 'nbGenes'
-  	    }),
-    project () {
-      return this.user.projects.filter(p => p.project_id === this.user.project_id)[0]
     },
-    options () {
-      return this.genes.map(g => { return {label: g.symbol, value: g.entrez_id} })
+    watch: {
+        gene: function (n, o) {
+            if (n.value) {
+                let geneIdx = _.findIndex(this.genes, g => {
+                    return g.entrez_id === n.value
+                })
+                let id = ''
+                if (geneIdx > -1) {
+                    let url = this.genes[geneIdx].url
+                    id = url.replace(serverURL + 'genes', '')
+                    id = id.replace(/\D/, '')
+                }
+                if (id) this.$router.push('gene/' + id)
+            }
+        }
     },
-    nbUniquePhenotypes () {
-      return _.uniqBy(this.phenotypes, d => { return d.pheno_id }).length
+    computed: {
+        ...mapGetters({
+            genes: 'genes',
+            variants: 'variants',
+            phenotypes: 'phenotypes',
+            nbVariants: 'nbVariants',
+            nbPhenotypes: 'nbPhenotypes',
+            nbGenes: 'nbGenes'
+        }),
+        project() {
+            return this.user.projects.filter(p => p.project_id === this.user.project_id)[0]
+        },
+        options() {
+            return this.genes.map(g => {
+                return {label: g.symbol, value: g.entrez_id}
+            })
+        },
+        nbUniquePhenotypes() {
+            return _.uniqBy(this.phenotypes, d => {
+                return d.pheno_id
+            }).length
+        }
+    },
+    methods: {
+        test(val) {
+            console.log(val)
+        }
+    },
+    created() {
+        store.dispatch('getSiteStats')
+        store.dispatch('getGenes')
+        store.dispatch('getVariants')
     }
-  },
-  methods: {
-    test (val) {
-      console.log(val)
-    }
-  },
-  created () {
-    const vm = this;
-    store.dispatch('getGenes')
-    store.dispatch('getVariants')
-    store.dispatch('getPhenotypes')
-  }
 
 }
 
@@ -97,25 +110,27 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.section{
-	margin-top: 60px;
+.section {
+    margin-top: 60px;
 }
-.intro{
-	margin-top: 120px;
+
+.intro {
+    margin-top: 120px;
 }
-.stats{
-	text-align: center;
+
+.stats {
+    text-align: center;
 }
 
 .d-center {
-  display: flex;
-  align-items: center;
+    display: flex;
+    align-items: center;
 }
 
 .selected img {
-  width: auto;
-  max-height: 23px;
-  margin-right: 0.5rem;
+    width: auto;
+    max-height: 23px;
+    margin-right: 0.5rem;
 }
 </style>
 /* eslint-disable */
