ff0476b0ce65faadbf9c27517cc07156e1e65d06
faisala <alquaddoomi@nexus.ethz.ch>
2019-02-14 11:36:04 +0100

    Suppress unused variable linting errors in development.

--- svip-o-vue/.eslintrc.js
+++ svip-o-vue/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-multiple-empty-lines': [2,{"max": 2, "maxEOF": 10000, "maxBOF": 10000}],
 		"indent": [2, "tab"],
 		"no-tabs": 0
