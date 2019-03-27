e87304c11ab99561f52823547c0cf8a332ca3424
faisala <alquaddoomi@nexus.ethz.ch>
2019-03-27 15:47:01 +0100

    Updates svip_variants with correct mock data. Improves mgmt command's resolution of the mock data file.

--- svip_api/api/management/commands/populate_mock_svipdata.py
+++ svip_api/api/management/commands/populate_mock_svipdata.py
@@ -1,12 +1,19 @@
 import sys
+import os
 import json
 from django.core.management.base import BaseCommand, CommandError
 from api.models import Variant, SVIPVariant
 
+import api
+
+
+APP_DIR = os.path.dirname(api.__file__)
+SVIP_VARS_JSON = os.path.join(APP_DIR, "static", "svip_variants.json")
+
 
 def create_svipvariants(model_variant, model_svip_variant):
     # loads variant info from SVIP mock data file into the db
-    with open("./api/static/svip_variants.json") as fp:
+    with open(SVIP_VARS_JSON, "r") as fp:
         svip_variants = json.load(fp)
 
         model_svip_variant.objects.all().delete()
