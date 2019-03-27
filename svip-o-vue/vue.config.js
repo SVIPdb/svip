b0aef9fe1f0ffd57876fc6e79227f1cdd62e1831
faisala <alquaddoomi@nexus.ethz.ch>
2019-03-27 13:42:16 +0100

    Adds a step to the build process that writes out the git-ref and version to the distributable.

--- svip-o-vue/vue.config.js
+++ svip-o-vue/vue.config.js
@@ -3,9 +3,17 @@ const pkg_json = require('./package.json');
 process.env.VUE_APP_VERSION = pkg_json.version;
 process.env.VUE_APP_RELEASE_NAME = pkg_json.release_name;
 
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+
 module.exports = {
 	runtimeCompiler: true,
 	transpileDependencies: [
 		/\bvue-awesome\b/
-	]
-}
+	],
+	configureWebpack: {
+		plugins: [
+			new GitRevisionPlugin({ branch: true })
+		]
+	}
+};
+
