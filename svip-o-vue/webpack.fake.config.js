e3596ecfe8890981c281ff82de663b8b2780c95b
faisala <alquaddoomi@nexus.ethz.ch>
2019-06-05 11:21:39 +0200

    Added sass-loader so we can use sass/scss. Added fake webpack config to make code navigation work in Webstorm.

---
+++ svip-o-vue/webpack.fake.config.js
@@ -0,0 +1,11 @@
+/* globals module, require, __dirname */
+const path = require('path');
+
+module.exports = {
+	resolve: {
+		extensions: ['.js', '.json', '.vue'],
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	}
+};
