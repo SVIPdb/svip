9ec4b4e8f95d05399bcfbe4278bd15ce6f67212c
faisala <alquaddoomi@nexus.ethz.ch>
2019-06-12 10:22:56 +0200

    Added a bit of debugging output to db init script.

--- db_config/populate.sh
+++ db_config/populate.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+REFERENCE_DUMP="/backups/svip_api_debug_2019-05-24.dump"
+
+echo "Creating reference database from ${REFERENCE_DUMP}"
 createdb svip_api
-pg_restore -d svip_api /backups/svip_api_debug_2019-05-24.dump
+pg_restore -d svip_api ${REFERENCE_DUMP}
