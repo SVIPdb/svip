e0aff299dc2b0c8931805493d1debd20ba2a2ce6
faisala <alquaddoomi@nexus.ethz.ch>
2019-10-03 11:51:45 +0200

    Adds 'exclude' option to VariantExternalInfo, in case we want to conditionally hide some subpanels in certain views.

--- svip-o-vue/src/components/genes/variants/external/VariantExternalInfo.vue
+++ svip-o-vue/src/components/genes/variants/external/VariantExternalInfo.vue
@@ -1,10 +1,10 @@
 <template>
-	<div class="row">
-		<Pathogenicity :cadd="mvInfo.cadd" :dbnsfp="mvInfo.dbnsfp" :extras="extras" v-if="mvInfo && (mvInfo.cadd || mvInfo.dbnsfp || extras)"/>
-		<PopulationStats :mvInfo="mvInfo" v-if="mvInfo"/>
-    <SwissPO :protein="variant.gene.symbol" :uniprot-id="variant.gene.uniprot_ids[0]" :change="variant.name" />
-    <SOCIBP :protein="variant.gene.symbol" :change="variant.name" />
-	</div>
+  <div class="row">
+    <Pathogenicity v-if="included('Pathogenicity') && mvInfo && (mvInfo.cadd || mvInfo.dbnsfp || extras)" :cadd="mvInfo.cadd" :dbnsfp="mvInfo.dbnsfp" :extras="extras" />
+    <PopulationStats v-if="included('PopulationStats') && mvInfo" :mvInfo="mvInfo" />
+    <SwissPO v-if="included('SwissPO')" :protein="variant.gene.symbol" :uniprot-id="variant.gene.uniprot_ids[0]" :change="variant.name" />
+    <SOCIBP v-if="included('SOCIBP')" :protein="variant.gene.symbol" :change="variant.name" />
+  </div>
 </template>
 
 <script>
@@ -14,9 +14,15 @@ import SwissPO from "@/components/genes/variants/external/SwissPO";
 import SOCIBP from "@/components/genes/variants/external/SOCIBP";
 
 export default {
-	name: "VariantExternalInfo",
-	components: {SwissPO, Pathogenicity, PopulationStats, SOCIBP},
-	props: ["variant", "mvInfo", "extras"]
+    name: "VariantExternalInfo",
+    components: {SwissPO, Pathogenicity, PopulationStats, SOCIBP},
+    props: ["variant", "mvInfo", "extras", "exclude"],
+    methods: {
+        included(componentName) {
+            // returns true if no exclusions are specified, or if this component wasn't named in the exclusion list
+            return !this.exclude || !this.exclude.includes(componentName);
+        }
+    }
 };
 </script>
 
