1255b0a7e7c781f3aabd7501adf3847594b5f992
faisala <alquaddoomi@nexus.ethz.ch>
2019-11-11 10:49:45 +0100

    Moves JWT auth above session auth, which allows API requests to clear w/out a CSRF token, but still allows the browseable API's login to function (i.e., via session auth).

--- svip_api/svip_server/settings/base.py
+++ svip_api/svip_server/settings/base.py
@@ -168,9 +168,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'api.pagination.StandardResultsSetPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
