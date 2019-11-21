46986256882cf96525461f0cdf758f722ef8ac7a
faisala <alquaddoomi@nexus.ethz.ch>
2019-11-21 14:57:01 +0100

    Adds new authorization meta to routes, including role-based authorization. Skips credential checks if requiresAuth is false, speeding up public page loading somewhat.

--- svip-o-vue/src/router/index.js
+++ svip-o-vue/src/router/index.js
@@ -14,6 +14,7 @@ import CurationDashboard from "@/views/curation/CurationDashboard";
 import AnnotateVariant from "@/views/curation/AnnotateVariant";
 import AddEvidence from "@/views/curation/AddEvidence";
 import DebugPage from "@/views/DebugPage";
+import {checkInRole} from "@/directives/access";
 
 Vue.use(Router);
 
@@ -51,17 +52,20 @@ const router = new Router({
         {
             path: "/curation/dashboard",
             name: "curation-dashboard",
-            component: CurationDashboard
+            component: CurationDashboard,
+            meta: { requiresAuth: true, roles: ['curators', 'reviewers'] }
         },
         {
             path: "/curation/gene/:gene_id/variant/:variant_id/disease/:disease_id",
             name: "annotate-variant",
-            component: AnnotateVariant
+            component: AnnotateVariant,
+            meta: { requiresAuth: true, roles: ['curators', 'reviewers'] }
         },
         {
             path: "/curation/gene/:gene_id/variant/:variant_id/disease/:disease_id/entry/:action",
             name: "add-evidence",
-            component: AddEvidence
+            component: AddEvidence,
+            meta: { requiresAuth: true, roles: ['curators', 'reviewers'] }
         },
         {
             path: "/debug",
@@ -79,18 +83,37 @@ const router = new Router({
 });
 
 router.beforeEach((to, from, next) => {
-    // FIXME: only check credentials if the route includes authorized content; if not, we'll get rejected if we have invalid creds even for public routes
-    // (we'd probably do that by adding a 'requiresAuth' and/or 'groups' fields to the route definitions)
-    store.dispatch("checkCredentials").then((result) => {
-        if (!result.valid && (result.reason === TokenErrors.EXPIRED || result.reason === TokenErrors.REFRESH_EXPIRED) && to.name !== "login") {
-            // if our token's expired, go get a new one from the login page,
-            // remembering where we eventually want to go to as well
-            next({ name: 'login', params: { default_error_msg: "Token expired, please log in again", nextRoute: to.path } });
-        }
-        else {
-            next();
-        }
-    });
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        // only check credentials if the route includes authorized content; if not, we'll get rejected if we have invalid creds even for public routes
+        // (we'd probably do that by adding a 'requiresAuth' and/or 'groups' fields to the route definitions)
+
+        const possibleRoles = to.matched.reduce((acc, record) => {
+            return record.meta.roles ? acc.concat(record.meta.roles) : acc;
+        }, []);
+
+        console.log("Requires auth, roles?: ", possibleRoles);
+
+        store.dispatch("checkCredentials").then((result) => {
+            if (!result.valid && (result.reason === TokenErrors.EXPIRED || result.reason === TokenErrors.REFRESH_EXPIRED) && to.name !== "login") {
+                // if our token's expired, go get a new one from the login page,
+                // remembering where we eventually want to go to as well
+                next({ name: 'login', params: { default_error_msg: "Token expired, please log in again", nextRoute: to.path } });
+            }
+            else if (possibleRoles && !possibleRoles.some(x => checkInRole(x))) {
+                console.log("Roles check failed!");
+                next({ name: 'login', params: {
+                    default_error_msg: `You don't have access to that resource (required role(s): ${possibleRoles.join(", ")}).`,
+                    nextRoute: to.path
+                }});
+            }
+            else {
+                next();
+            }
+        });
+    }
+    else {
+        next(); // just let them proceed
+    }
 });
 
 export default router;
