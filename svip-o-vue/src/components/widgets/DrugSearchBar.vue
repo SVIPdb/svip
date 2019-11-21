f785712b4088e0f455a2610de8d05162fb36424f
faisala <alquaddoomi@nexus.ethz.ch>
2019-11-21 14:54:31 +0100

    Updates invalidated CSS style for new namespaced vue-select classes.

--- svip-o-vue/src/components/widgets/DrugSearchBar.vue
+++ svip-o-vue/src/components/widgets/DrugSearchBar.vue
@@ -40,15 +40,8 @@ export default {
 </script>
 
 <style scoped>
-#my-select >>> .dropdown-toggle { padding: 24px; }
-.invalidated >>> .dropdown-toggle  {
+#my-select >>> .vs__dropdown-toggle { padding: 24px; }
+.invalidated >>> .vs__dropdown-toggle  {
     border-color: #e74c3c !important;
-
-    /*
-    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='%23dc3545' viewBox='-2 -2 7 7'%3e%3cpath stroke='%23dc3545' d='M0 0l3 3m0-3L0 3'/%3e%3ccircle r='.5'/%3e%3ccircle cx='3' r='.5'/%3e%3ccircle cy='3' r='.5'/%3e%3ccircle cx='3' cy='3' r='.5'/%3e%3c/svg%3E");
-    background-repeat: no-repeat;
-    background-position: center right calc(1.5em + .375rem);
-    background-size: calc(.75em + .375rem) calc(.75em + .375rem);
-     */
 }
 </style>
