510b495169e0377de1c957840479ccbc0da237eb
faisala <alquaddoomi@nexus.ethz.ch>
2019-11-21 14:49:23 +0100

    Adds variant_only boolean flag to query endpoint, forcing it to only query variants and not genes

--- svip_api/api/views/query.py
+++ svip_api/api/views/query.py
@@ -21,33 +21,40 @@ class QueryView(viewsets.ViewSet):
         resp = []
         search_term = request.GET.get('q', None)
         in_svip = (request.GET.get('in_svip', False) == 'true')
+        variants_only = (request.GET.get('variants_only', False) == 'true')
 
         if search_term is not None and search_term != '':
             # convert full amino acids to their single-letter abbreviations, since that's how they're stored in the
             # database; e.g., 'Val600Glu' becomes 'V600E'
             collapsed_search = reduce(lambda acc, v: v[0].sub(v[1], acc), three_to_one_icase.items(), search_term)
 
-            gq = Gene.objects.filter(
-                Q(symbol__icontains=search_term) | Q(aliases__icontains=search_term)
-            )
             vq = Variant.objects.filter(
                 Q(description__icontains=search_term) | Q(hgvs_c__icontains=search_term) |
                 Q(name__contains=collapsed_search)
             ).select_related('gene')
 
             if in_svip:
-                gq = gq.annotate(svip_vars=Count('variant__variantinsvip')).filter(svip_vars__gt=0)
                 vq = vq.filter(variantinsvip__isnull=False)
 
-            g_resp = list({
-                'id': x.id,
-                'type': 'g',
-                'label': "%s (aka: %s)" % (x.symbol, ", ".join(x.aliases))
-            } for x in gq)
             v_resp = list(format_variant(x) for x in vq)
 
-            resp = g_resp + v_resp
-        else:
+            if not variants_only:
+                gq = Gene.objects.filter(Q(symbol__icontains=search_term) | Q(aliases__icontains=search_term))
+
+                if in_svip:
+                    gq = gq.annotate(svip_vars=Count('variant__variantinsvip')).filter(svip_vars__gt=0)
+
+                g_resp = list({
+                    'id': x.id,
+                    'type': 'g',
+                    'label': "%s (aka: %s)" % (x.symbol, ", ".join(x.aliases))
+                } for x in gq)
+
+                resp = g_resp + v_resp
+            else:
+                resp = v_resp
+
+        elif not variants_only:
             # send back the full list of genes
             gq = Gene.objects.all()
 
@@ -60,6 +67,9 @@ class QueryView(viewsets.ViewSet):
                 'label': x.symbol
             } for x in gq)
             resp = g_resp
+        else:
+            # they're not querying for anything, but they don't want genes, so we can't return them anything
+            resp = []
 
         return Response(resp)
 
