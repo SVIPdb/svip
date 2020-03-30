6d69ead956b0f69680f411970f39ba95a120ead9
faisala <alquaddoomi@nexus.ethz.ch>
2020-03-30 15:39:56 +0200

    Created a docker-compose dispatcher that'll work on all our hosts. Removed previous launcher.

---
+++ dc_host.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+case $(hostname) in
+  "svip-dev")
+    SITE_COMPOSE="-f compose_configs/sites/svip-dev.yml"
+    ;;
+  "svip-test")
+    SITE_COMPOSE="-f compose_configs/sites/svip-test.yml"
+    ;;
+  "svip.nexus.ethz.ch")
+    SITE_COMPOSE="-f compose_configs/sites/svip.yml"
+    ;;
+  *)
+    SITE_COMPOSE=""
+esac
+
+docker-compose -f docker-compose.yml -f compose_configs/prod.yml ${SITE_COMPOSE} "$@"
