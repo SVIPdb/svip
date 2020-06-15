fe726cca76aba95a6dd0b063ff5d4483826b6005
faisala <alquaddoomi@nexus.ethz.ch>
2020-06-15 16:47:56 +0200

    Removes auto-loading db from postgres service config

--- service_config/postgres/populate.sh
+++ service_config/postgres/populate.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-TARGET_DB="svip_api"
-REFERENCE_DUMP="/backups/svip_api_2020-02-12.dump"
+# TARGET_DB="svip_api"
+# REFERENCE_DUMP="/backups/svip_api_2020-02-12.dump"
 
-echo "Creating reference database from ${REFERENCE_DUMP}"
-dropdb ${TARGET_DB}
-createdb ${TARGET_DB}
-pg_restore -F c -d ${TARGET_DB} ${REFERENCE_DUMP}
+# echo "Creating reference database from ${REFERENCE_DUMP}"
+# dropdb ${TARGET_DB}
+# createdb ${TARGET_DB}
+# pg_restore -F c -d ${TARGET_DB} ${REFERENCE_DUMP}
+
+# NOTE: users should manualy populate the database to avoid deadlocks at boot
+# you can load it yourself like so: ./svip_db_backups/load_default_dump.sh
