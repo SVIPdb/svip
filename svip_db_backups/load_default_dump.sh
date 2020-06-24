51535fa4ee866a0ea82cdf4089cef086ec80b748
faisala <alquaddoomi@nexus.ethz.ch>
2020-06-24 10:40:44 +0200

    Allows load_default_dump to run in any cluster installation, other minor tweaks.

--- svip_db_backups/load_default_dump.sh
+++ svip_db_backups/load_default_dump.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-echo "=> Loading SVIP db from svip_api_2020-02-12.dump; this may take some time (8+ minutes)..."
-time ( docker stop svip_api_1 && docker exec -it svip_db_1 bash -c 'dropdb svip_api; createdb svip_api && pg_restore -F c -d svip_api /backups/svip_api_2020-02-12.dump' )
+API_CONTAINER=$( docker-compose ps | grep api | cut -d ' ' -f1 )
+DB_CONTAINER=$( docker-compose ps | grep db | cut -d ' ' -f1 )
+DEFAULT_DUMPFILE="/backups/svip_api_2020-02-12.dump"
+TARGET_DB="svip_api"
 
+if [ -z "${DB_CONTAINER}" ]; then
+    echo "ERROR: can't find database container, aborting"
+    exit 1
+fi
+
+echo "=> Loading SVIP db from ${DEFAULT_DUMPFILE} into ${DB_CONTAINER}; this may take some time (~8+ minutes)..."
+docker exec -it ${DB_CONTAINER} bash -c "/backups/restoredump.sh ${DEFAULT_DUMPFILE} ${TARGET_DB}"
+
+# run migrations in API container, if present
+if [ ! -z "${API_CONTAINER}" ]; then
+    echo "=> API container '${API_CONTAINER}' found, migrating..."
+    docker exec -it ${API_CONTAINER} '/app/manage.py migrate'
+fi
