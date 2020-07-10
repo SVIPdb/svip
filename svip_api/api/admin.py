a749788606e79ba40ef616ae8dd1a556b3301e34
faisala <alquaddoomi@nexus.ethz.ch>
2020-07-10 10:37:38 +0200

    Adds admin views for managing variants, VarInSVIP, curation entries.

--- svip_api/api/admin.py
+++ svip_api/api/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import VariantInSVIP, DiseaseInSVIP, Variant, CurationEntry
+
+
+@admin.register(Variant)
+class VariantAdmin(admin.ModelAdmin):
+    list_filter = ('gene__symbol',)
+    ordering = ('gene__symbol', 'name')
+    search_fields = ('gene__symbol', 'name', 'hgvs_c')
+
+@admin.register(VariantInSVIP)
+class VariantInSVIPAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['variant']
+
+admin.site.register(DiseaseInSVIP)
+
+@admin.register(CurationEntry)
+class CurationEntryAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['variant']
