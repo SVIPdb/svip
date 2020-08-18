b74245134c54adc97faff117c3b2377bdf8933d0
faisala <alquaddoomi@nexus.ethz.ch>
2020-08-18 17:04:13 +0200

    Changes swiss-po requests to HTTPS.

--- svip_api/api/views/proxies/swisspo_proxy.py
+++ svip_api/api/views/proxies/swisspo_proxy.py
@@ -34,7 +34,7 @@ def swisspo_request(request, path):
     payload = (request.GET if request.method == 'GET' else request.POST).copy()
     payload['get_option'] = 'BRAF'
 
-    target_url = 'http://swiss-po.ch/%s' % path
+    target_url = 'https://swiss-po.ch/%s' % path
 
     resp = cached_sess.request(
         method='POST',
@@ -63,17 +63,17 @@ def swisspo_request(request, path):
 # -------------------------------------------------------------
 
 # the pipeline consists of the following steps:
-#  1) POST to http://swiss-po.ch/includes/main/fetch_data_pdb.php {get_option: BRAF} to get HTML dropdown of PDBs for this gene
-#  2) POST to http://swiss-po.ch/includes/main/fetch_data_link.php {get_option: BRAF} to get links to uni/nexprot for this gene
-#  3) POST to http://swiss-po.ch/includes/main/fetch_data_pdb_table.php {option_protein: BRAF, option_threshold: 9} to get
+#  1) POST to https://swiss-po.ch/includes/main/fetch_data_pdb.php {get_option: BRAF} to get HTML dropdown of PDBs for this gene
+#  2) POST to https://swiss-po.ch/includes/main/fetch_data_link.php {get_option: BRAF} to get links to uni/nexprot for this gene
+#  3) POST to https://swiss-po.ch/includes/main/fetch_data_pdb_table.php {option_protein: BRAF, option_threshold: 9} to get
 #     some kind of response to put in a "select PDB" panel that we never actually see...?
-#  4) POST to http://swiss-po.ch/includes/main/fetch_data_fasta.php {get_option: BRAF} to get the name of the FASTA file
-#  5) POST to http://swiss-po.ch/includes/main/fetch_residueNumbers.php {option_pdbId: 5HIE, option_chain: A} to get
+#  4) POST to https://swiss-po.ch/includes/main/fetch_data_fasta.php {get_option: BRAF} to get the name of the FASTA file
+#  5) POST to https://swiss-po.ch/includes/main/fetch_residueNumbers.php {option_pdbId: 5HIE, option_chain: A} to get
 #     something that looks like "449-722??separator??BRAF/PDB/5HIE.A.prot_corrected" (using the first PDB from step 1 or the contents of a cookie)
-#  6) POST to http://swiss-po.ch/includes/main/fetch_data_pdb_info3D.php {get_option: 5HIE} to get an HTML blob
+#  6) POST to https://swiss-po.ch/includes/main/fetch_data_pdb_info3D.php {get_option: 5HIE} to get an HTML blob
 #     about the PDB to display in the tooltip above NGL.
-#  7) GET http://swiss-po.ch/data/BRAF/MUSCLE_BRAF_ordered_aligned.fasta to get FASTA data (using name from step 4)
-#  8) GET http://swiss-po.ch/data/BRAF/PDB/5HIE.A.prot_corrected.pdb to get PDB (using name from step 5)
+#  7) GET https://swiss-po.ch/data/BRAF/MUSCLE_BRAF_ordered_aligned.fasta to get FASTA data (using name from step 4)
+#  8) GET https://swiss-po.ch/data/BRAF/PDB/5HIE.A.prot_corrected.pdb to get PDB (using name from step 5)
 
 
 def get_pdbs(request, protein):
@@ -83,7 +83,7 @@ def get_pdbs(request, protein):
     :param protein: a HUGO gene symbol, e.g. "BRAF"
     :return: an array of {pdb, chain} objects identifying the RCSB PDB ID and relevant chains for that protein
     """
-    response = requests.post('http://swiss-po.ch/includes/main/fetch_data_pdb.php', data={
+    response = requests.post('https://swiss-po.ch/includes/main/fetch_data_pdb.php', data={
         'get_option': protein
     })
 
@@ -104,7 +104,7 @@ def get_residues(request, pdb_id, chain):
     :param chain: a chain identifier, e.g. "A"
     :return: an array of {pdb, chain} objects identifying the RCSB PDB ID and relevant chains for that protein
     """
-    response = requests.post('http://swiss-po.ch/includes/main/fetch_residueNumbers.php', data={
+    response = requests.post('https://swiss-po.ch/includes/main/fetch_residueNumbers.php', data={
         'option_pdbId': pdb_id, 'option_chain': chain
     })
 
@@ -123,7 +123,7 @@ def get_residues(request, pdb_id, chain):
 
 
 def get_pdb_data(request, pdb_path):
-    response = requests.get('http://swiss-po.ch/data/%s.pdb' % pdb_path)
+    response = requests.get('https://swiss-po.ch/data/%s.pdb' % pdb_path)
 
     if response.status_code != 200:
         return HttpResponse(response.content, status=response.status_code)
