c89d3a6a6996c34311acdaed0b0f76339ca20e6c
faisala <alquaddoomi@nexus.ethz.ch>
2020-09-04 13:03:11 +0200

    Adds DJANGO_SU_* to admin list, to be notified in case of errors.

--- svip_api/svip_server/settings/production.py
+++ svip_api/svip_server/settings/production.py
@@ -1,3 +1,9 @@
 from .base import *
 
 DEBUG = False
+
+# put in some admins who should receive emails if things go wrong
+if 'DJANGO_SU_NAME' in os.environ and 'DJANGO_SU_EMAIL' in os.environ:
+    ADMINS = [(
+        os.environ['DJANGO_SU_NAME'], os.environ['DJANGO_SU_EMAIL']
+    )]
