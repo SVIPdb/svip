9d7055c965e50e6a93ca118e8f080808148f6181
faisala <alquaddoomi@nexus.ethz.ch>
2020-09-04 13:29:18 +0200

    Adds logging to /var/log/django.log to the production instances.

--- svip_api/svip_server/settings/production.py
+++ svip_api/svip_server/settings/production.py
@@ -7,3 +7,34 @@ if 'DJANGO_SU_NAME' in os.environ and 'DJANGO_SU_EMAIL' in os.environ:
     ADMINS = [(
         os.environ['DJANGO_SU_NAME'], os.environ['DJANGO_SU_EMAIL']
     )]
+
+# enable logging, too
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": "INFO", "handlers": ["file"]},
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "/var/log/django.log",
+            "formatter": "app",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True
+        },
+    },
+    "formatters": {
+        "app": {
+            "format": (
+                u"%(asctime)s [%(levelname)-8s] "
+                "(%(module)s.%(funcName)s) %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+}
