f2b8ba4e496e7c9f1c1eeb4a98a3e64f1c208d0d
faisala <alquaddoomi@nexus.ethz.ch>
2020-09-21 13:00:08 +0200

    Added fields, filters to CurationEntryAdmin. Integrated w/SimpleHistory.

--- svip_api/api/admin.py
+++ svip_api/api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 
-# Register your models here.
 from api.models import VariantInSVIP, DiseaseInSVIP, Variant, CurationEntry
 
 
@@ -17,5 +17,34 @@ class VariantInSVIPAdmin(admin.ModelAdmin):
 admin.site.register(DiseaseInSVIP)
 
 @admin.register(CurationEntry)
-class CurationEntryAdmin(admin.ModelAdmin):
+class CurationEntryAdmin(SimpleHistoryAdmin):
+    search_fields = (
+        'disease__name',
+        'variant__description',
+        'type_of_evidence',
+        'drugs',
+        'interactions',
+        'effect',
+        'tier_level_criteria',
+        'tier_level',
+        'mutation_origin',
+        'associated_mendelian_diseases',
+        'summary',
+        'support',
+        'comment',
+        'references',
+        'annotations',
+        'owner__username'
+    )
+    list_display = (
+        'id', 'status',
+        'variant',
+        'type_of_evidence',
+        'tier_level',
+        'tier_level_criteria',
+        'mutation_origin',
+        'owner'
+    )
+    list_select_related = ('variant', 'owner',)
+    list_filter = ('status', 'owner', 'type_of_evidence')
     autocomplete_fields = ['variant']
