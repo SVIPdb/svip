a1c6fe048b4c651c4c21170fba1a6a999d8eac41
faisala <alquaddoomi@nexus.ethz.ch>
2020-09-22 14:55:15 +0200

    CurationDashboard: replaces extremely slow query for all variants w/SVIP data with a fast one for curation entries. Factors out request queue into OnRequestEntries, which wraps NotificationCard.

--- svip-o-vue/src/components/views/curation/CurationDashboard.vue
+++ svip-o-vue/src/components/views/curation/CurationDashboard.vue
@@ -40,9 +40,7 @@
 
         <div v-else-if="checkInRole('curators')">
             <!-- TBC: request queue -->
-            <NotificationCard
-                :items="on_request.items" :fields="on_request.fields" :loading="on_request.loading"
-                :error="on_request.error"
+            <OnRequestEntries
                 defaultSortBy="days_left"
                 title="ON REQUEST"
                 cardHeaderBg="secondary"
@@ -68,14 +66,9 @@
 </template>
 
 <script>
-import { HTTP } from "@/router/http";
-import flatMap from 'lodash/flatMap';
-import uniqBy from 'lodash/uniqBy';
 import NotificationCard from "@/components/widgets/curation/NotificationCard";
 import EvidenceCard from "@/components/widgets/curation/EvidenceCard";
 import { checkInRole } from "@/directives/access";
-// Manual import of fake data (FIXME: API)
-import fields_on_request from "@/data/curation/on_request/fields.js";
 
 import to_be_curated from "@/data/curation/to_be_curated/items.json";
 import fields_to_be_curated from "@/data/curation/to_be_curated/fields.json";
@@ -85,24 +78,18 @@ import fields_to_be_discussed from "@/data/curation/to_be_discussed/fields.json"
 
 import nonsvip_variants from "@/data/curation/nonsvip_variants/items.json";
 import fields_nonsvip_variants from "@/data/curation/nonsvip_variants/fields.json";
+import OnRequestEntries from "@/components/widgets/curation/OnRequestEntries";
 
 export default {
     name: "CurationDashboard",
     components: {
+        OnRequestEntries,
         EvidenceCard,
         NotificationCard
     },
     data() {
         return {
             REVIEW_ENABLED: false, // temporary flag to hide review-related bits of the UI until they're ready
-
-            // ON REQUEST FAKE DATA
-            on_request: {
-                loading: false,
-                fields: fields_on_request,
-                items: [],
-                error: null
-            },
 
             // TO BE CURATED FAKE DATA
             to_be_curated, // data
@@ -117,40 +104,8 @@ export default {
             fields_nonsvip_variants // columns
         };
     },
-    created() {
-        this.fetchRequestedVariants();
-    },
     methods: {
-        checkInRole,
-        fetchRequestedVariants() {
-            this.$set(this.on_request, 'loading', true);
-            this.$set(this.on_request, 'error', null);
-            HTTP.get(`/variants?in_svip=true&inline_svip_data=true&page_size=10000`)
-                .then((response) => {
-                    this.on_request.loading = false;
-
-                    this.on_request.items = response.data.results.map((entry) => {
-                        const all_curations = flatMap(entry.svip_data.diseases, x => x.curation_entries);
-
-                        return {
-                            gene_id: entry.gene.id,
-                            variant_id: entry.id,
-                            'gene_name': entry.gene.symbol,
-                            'variant': entry.name,
-                            'hgvs': entry.hgvs_c,
-                            'disease': entry.svip_data.diseases.map(x => x.name).join(", "),
-                            'status': all_curations.length > 0 ? 'Ongoing' : 'Not assigned',
-                            'deadline': 'n/a',
-                            'requester': 'System',
-                            'curator': uniqBy(all_curations.map(x => ({ id: x.owner, name: x.owner_name })), (x) => x.id)
-                        };
-                    });
-                })
-                .catch((err) => {
-                    this.$set(this.on_request, 'loading', false);
-                    this.$set(this.on_request, 'error', err.message ? err.message : true);
-                })
-        }
+        checkInRole
     }
 };
 </script>
