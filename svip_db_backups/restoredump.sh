10d5381ce68d72eb57a0ce00c9d36606f1ccabba
faisala <alquaddoomi@nexus.ethz.ch>
2020-09-23 13:07:30 +0200

    Makes restoredump more robust to odd situations, e.g. the database not existing. Adds timing info output.

--- svip_db_backups/restoredump.sh
+++ svip_db_backups/restoredump.sh
@@ -5,19 +5,26 @@ function die() {
   exit -1
 }
 
-[ -z "$1" ] && die "Usage: $0 <dumpfile> [target_db=svip_api_debug]"
+[ -z "$1" ] && die "Usage: $0 <dumpfile> [target_db=svip_api]"
 
 if [ ! -f "$1" ]; then
   echo "Error: dump file '$1' doesn't exist or was not found"
   exit -1
 fi
 
-TARGET_DB=${2:-svip_api_debug}
+TARGET_DB=${2:-svip_api}
 
 # first, attempt to disconnect everyone
 echo "Attempting to disconnect existing users from ${TARGET_DB}..."
 psql -U postgres -c "SELECT pid, (SELECT pg_terminate_backend(pid)) as killed from pg_stat_activity WHERE datname = '${TARGET_DB}';"
 
-dropdb ${TARGET_DB} || die "Can't drop DB, presumably because it's in use"
+DROP_OUTPUT=$( dropdb ${TARGET_DB} )
+if [ $? -ne 0 ]; then
+  if [ -z "${DROP_OUTPUT##*does not exist*}" ]; then
+    echo "Drop failed b/c database didn't exist, continuing..."
+  else
+    die "Can't drop DB, presumably because it's in use"
+  fi
+fi
 
-createdb ${TARGET_DB}; pg_restore -j 16 -F c -d ${TARGET_DB} $1
+createdb ${TARGET_DB}; time pg_restore -j 16 -F c -d ${TARGET_DB} $1
