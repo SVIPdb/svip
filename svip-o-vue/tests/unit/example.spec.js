5587549f88e8fdc025daeb0eee2f81ac5c4903f2
faisala <alquaddoomi@nexus.ethz.ch>
2020-10-20 15:13:54 +0200

    Committed linting changes to testing files

--- svip-o-vue/tests/unit/example.spec.js
+++ svip-o-vue/tests/unit/example.spec.js
@@ -2,11 +2,11 @@ import { shallowMount } from '@vue/test-utils'
 import App from '@/App'
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(App, {
-      propsData: { msg }
+    it('renders props.msg when passed', () => {
+        const msg = 'new message'
+        const wrapper = shallowMount(App, {
+            propsData: { msg }
+        })
+        expect(wrapper.text()).toMatch(msg)
     })
-    expect(wrapper.text()).toMatch(msg)
-  })
 })
