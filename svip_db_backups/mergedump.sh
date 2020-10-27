2fdb2273f0e255b4f6ff72baf9a1c0392b494966
faisala <alquaddoomi@nexus.ethz.ch>
2020-10-27 08:19:13 +0100

    Updates mergedump script to handle genes/variants newly introduced in the incoming dump.

--- svip_db_backups/mergedump.sh
+++ svip_db_backups/mergedump.sh
@@ -32,7 +32,7 @@ function die() {
 
 if [ ! -f "$1" ]; then
   echo "Error: dump file '$1' doesn't exist or was not found"
-  exit -1
+  exit 1
 fi
 
 DUMPFILE="$1"
@@ -41,6 +41,13 @@ TARGET_DB=${2:-svip_api}
 # first, attempt to disconnect everyone
 echo "Attempting to disconnect existing users from ${TARGET_DB}..."
 psql -U postgres -c "SELECT pid, (SELECT pg_terminate_backend(pid)) as killed from pg_stat_activity WHERE datname = '${TARGET_DB}';"
+
+GENE_COLS="entrez_id, ensembl_gene_id, symbol, sources, uniprot_ids, location, aliases, prev_symbols"
+
+# used to contain gene_id
+VARIANT_COLS="id, name, description, biomarker_type, so_hierarchy, soid, sources, so_name, isoform,\
+refseq, chromosome, end_pos, hgvs_c, hgvs_p, reference_name, start_pos, alt, ref, hgvs_g, dbsnp_ids,\
+myvariant_hg19, mv_info, crawl_status, somatic_status"
 
 # first, we produce a script from the dump that does everything we need
 time (
@@ -56,9 +63,17 @@ time (
     pg_restore "${DUMPFILE}" -t api_gene -t api_variant \
       | sed 's/api_variant/api_variant_temp/g;s/api_gene/api_gene_temp/g'
 
-    # copy from the temp tables into the source tables, ignoring potential duplicates
-    echo 'insert into public.api_gene select * from public.api_gene_temp on conflict do nothing;' || die "Failed to populate gene table"
-    echo 'insert into public.api_variant select * from public.api_variant_temp on conflict do nothing;' || die "Failed to populate variant table"
+    # annotate the incoming variants with __symbol, so they can be mapped to new genes that we're inserting
+    echo "alter table public.api_variant_temp add column __symbol varchar;"
+    echo "update public.api_variant_temp VT set __symbol=(select symbol from public.api_gene_temp where id=VT.gene_id);"
+
+    # copy from the temp gene table into the target gene table, ignoring potential duplicates
+    echo "insert into public.api_gene (${GENE_COLS}) \
+    select ${GENE_COLS} from public.api_gene_temp on conflict do nothing;"
+
+    # do the same for variants, but this time with the variants' ids included
+    echo "insert into public.api_variant (gene_id, ${VARIANT_COLS}) \
+    select (select id from public.api_gene where symbol=VT.__symbol) as gene_id, ${VARIANT_COLS} from public.api_variant_temp VT on conflict do nothing;"
 
     # # copy evidence tables from dump into the db
     pg_restore "${DUMPFILE}" --data-only \
@@ -67,8 +82,8 @@ time (
     # remove temp tables
     echo 'drop table public.api_gene_temp; drop table public.api_variant_temp;'
 
-    # and finish it all
-    echo "COMMIT;" # echo "ROLLBACK;" # echo "COMMIT;"
+    # and persist the changes
+    echo "COMMIT;"
 
   ) | psql -v ON_ERROR_STOP=1 -U postgres -d "${TARGET_DB}"
 )
