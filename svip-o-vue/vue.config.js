b8257c7e5e4c1a5c62ce8ce614622f140cf1f50e
dameyerdave <dameyerdave@gmail.com>
2020-12-09 09:14:28 +0100

    removed gitrevisionplugin

--- svip-o-vue/vue.config.js
+++ svip-o-vue/vue.config.js
@@ -3,18 +3,18 @@ const pkg_json = require('./package.json');
 process.env.VUE_APP_VERSION = pkg_json.version;
 process.env.VUE_APP_RELEASE_NAME = pkg_json.release_name;
 
-const GitRevisionPlugin = require('git-revision-webpack-plugin');
+// const GitRevisionPlugin = require('git-revision-webpack-plugin');
 
 module.exports = {
     runtimeCompiler: true,
     transpileDependencies: [
         /\bvue-awesome\b/
     ],
-    configureWebpack: {
-        plugins: [
-            new GitRevisionPlugin({ branch: true })
-        ]
-    },
+    // configureWebpack: {
+    //     plugins: [
+    //         new GitRevisionPlugin({ branch: true })
+    //     ]
+    // },
     chainWebpack: config => {
         config.plugins.delete('progress')
     }
