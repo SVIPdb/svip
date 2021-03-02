26f11f4138c92a68986eb79c738c7942f21eb61c
dameyerdave <dameyerdave@gmail.com>
2021-03-02 08:49:42 +0100

    added -U postgres to the makedump

--- svip_db_backups/makedump.sh
+++ svip_db_backups/makedump.sh
@@ -2,4 +2,4 @@
 
 TARGET_DB=${1:-svip_api}
 
-pg_dump -F c -d ${TARGET_DB} > ${TARGET_DB}_$( date +"%Y-%m-%d" ).dump
+pg_dump -U postgres -F c -d ${TARGET_DB} > ${TARGET_DB}_$( date +"%Y-%m-%d" ).dump
