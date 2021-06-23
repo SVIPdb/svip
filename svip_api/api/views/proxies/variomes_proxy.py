4cbacd275d13cc37a1bccfe779e9fa0ee5db08f1
dameyerdave <dameyerdave@gmail.com>
2021-06-23 09:03:51 +0200

    merged variomes proxy

--- svip_api/api/views/proxies/variomes_proxy.py
+++ svip_api/api/views/proxies/variomes_proxy.py
@@ -31,17 +31,10 @@ def variomes_single_ref(request):
 def variomes_search(request):
 
     # proxy requests to variomes server
-<< << << < HEAD
-   api = settings.VARIOMES_API if hasattr(
-        settings, 'VARIOMES_API') else 'https://candy.hesge.ch/Variomes/api'
-    response = cached_sess.get(
-        f"{api}/rankLit2.jsp", params=request.GET, verify=False)
-== == == =
-   response = cached_sess.get('%s/rankLit.jsp' % settings.VARIOMES_BASE_URL,
-                              params=request.GET, verify=settings.VARIOMES_VERIFY_REQUESTS)
->>>>>> > 4c4aedb508d74a158f8c6e65c331127d70cb139f
+    response = cached_sess.get('%s/rankLit.jsp' % settings.VARIOMES_BASE_URL,
+                               params=request.GET, verify=settings.VARIOMES_VERIFY_REQUESTS)
 
-   if response.status_code != 200:
+    if response.status_code != 200:
         return HttpResponse(response.content, status=response.status_code)
 
     return JsonResponse(response.json())
