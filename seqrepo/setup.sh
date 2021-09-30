e4505000503f7dcaaff136b3797c02f053655f76
dameyerdave <dameyerdave@gmail.com>
2021-09-30 15:25:11 +0200

    fixed seqrepo setup

--- seqrepo/setup.sh
+++ seqrepo/setup.sh
@@ -3,4 +3,4 @@ USER=$(echo "$(id -u):$(id -g)")
 
 mkdir -p -m 777 /usr/local/share/seqrepo	
 docker-compose run --rm -u ${USER} seqrepo 
-ln -s /usr/local/share/seqrepo/$(find /usr/local/share/seqrepo -type d -maxdepth 1 | sort | tail -1 | sed -E 's,.*\/,,') /usr/local/share/seqrepo/latest
+ln -s /usr/local/share/seqrepo/$(find /usr/local/share/seqrepo -maxdepth 1 -type d | sort | tail -1 | sed -E 's,.*\/,,') /usr/local/share/seqrepo/latest
