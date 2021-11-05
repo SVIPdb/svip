cf6cc75bf6d71be8be302fcefe6f3fcb7d00f70d
Gautier <gautier@nexus.ethz.ch>
2021-11-05 11:49:12 +0100

    command for cleaning diseases in database seems to work

--- svip_api/api/management/commands/clean_disease_references.py
+++ svip_api/api/management/commands/clean_disease_references.py
@@ -3,14 +3,24 @@ from api.models.reference import Disease
 from api.models.icdo import IcdOTopoApiDisease
 from api.models.svip import DiseaseInSVIP
 
-class Command(BaseCommand):
-  help = 'Gets rid of duplidated diseases and attribute the associated data to the disease of reference'
 
-  def handle(self, *args, **options):
+def clean_diseases_in_svip():
+    for svip_disease in DiseaseInSVIP.objects.all():
+        current_queryset = DiseaseInSVIP.objects.filter(svip_variant=svip_disease.svip_variant, disease=svip_disease.disease)
+        if len(current_queryset) > 1:
+            svip_disease_to_keep = current_queryset.first()
+            for duplicate in current_queryset.all():
+                if duplicate != svip_disease_to_keep:
+                    for sample in duplicate.sample_set.all():
+                        sample.disease_in_svip = svip_disease_to_keep
+                        sample.save()
+                    print(f"svip_disease {duplicate.id} about to be deleted")
+                    duplicate.delete()
+
+
+def clean_diseases():
     diseases = {}
     for disease in Disease.objects.all():
-
-        print(disease.id)
 
         if len(disease.icdotopoapidisease_set.all()) > 0:
             topo_list = []
@@ -30,8 +40,7 @@ class Command(BaseCommand):
             # if there is already a disease ID for this set of topo IDs: pass the information of current disease to that other disease, and delete current disease
             if topo_str in diseases[disease.icd_o_morpho.id]:
                 disease_to_keep = Disease.objects.get(id=diseases[disease.icd_o_morpho.id][topo_str])
-                
-                print(f"disease {disease.id} is a duplicate of disease {disease_to_keep}")
+                print(f"\ndisease {disease.id} is a duplicate of disease {disease_to_keep}")
 
                 for association in disease.curation_associations.all():
                     association.disease = disease_to_keep
@@ -46,23 +55,35 @@ class Command(BaseCommand):
                     submitted_var.save()
 
                 for svip_disease in disease.diseaseinsvip_set.all():
-                    svip_disease.disease = disease_to_keep
-                    print(disease_to_keep.id)
-                    svip_disease.save()
+                    
+                    # make sure the edited disease in svip won't have the same params as an arealdy exsiting one
+                    current_queryset = DiseaseInSVIP.objects.filter(svip_variant=svip_disease.svip_variant, disease=disease_to_keep)
+                    if len(current_queryset) > 0:
+                        svip_disease_to_keep = current_queryset.first()
+                        for sample in svip_disease.sample_set.all():
+                            sample.disease_in_svip = svip_disease_to_keep
+                            sample.save()
+                        print(f"svip_disease {svip_disease.id} about to be deleted")
+                        svip_disease.delete()
+                    else:
+                        svip_disease.disease = disease_to_keep
+                        svip_disease.save()
 
-                print('flag 1')
                 for curation in disease.curationentry_set.all():
-                    print(f'curation detected: ID is {curation.id}')
-                    print(f"disease of curation should become {disease_to_keep.id}")
                     curation.disease = disease_to_keep
                     curation.save()
-                print('flag 2')
 
                 disease.delete()
-                print('disease has been deleted.')
+                print('disease has been deleted.\n')
 
             else:
                 diseases[disease.icd_o_morpho.id][topo_str] = disease.id
                 print(f"disease {disease.id} is stored.")
 
-    print(diseases)
+
+class Command(BaseCommand):
+    help = 'Gets rid of duplidated diseases and attribute the associated data to the disease of reference'
+
+    def handle(self, *args, **options):
+        clean_diseases_in_svip()
+        clean_diseases()
