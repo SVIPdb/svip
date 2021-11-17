f9d11021e4d1562047d443819d610b18b4022fbe
dameyerdave <dameyerdave@gmail.com>
2021-11-17 08:38:10 +0100

    Changed version and name and take version string from git

--- svip-o-vue/vue.config.js
+++ svip-o-vue/vue.config.js
@@ -1,6 +1,6 @@
 /* globals process, module, require */
 const pkg_json = require('./package.json');
-process.env.VUE_APP_VERSION = pkg_json.version;
+// process.env.VUE_APP_VERSION = pkg_json.version;
 process.env.VUE_APP_RELEASE_NAME = pkg_json.release_name;
 
 const GitRevisionPlugin = require('git-revision-webpack-plugin');
@@ -17,6 +17,14 @@ module.exports = {
     },
     chainWebpack: config => {
         config.plugins.delete('progress')
+        config.plugin('define').tap(args => {
+            const gitRevisionPlugin = new GitRevisionPlugin()
+            args[0]['process.env']['VUE_APP_COMMITHASH'] = JSON.stringify(gitRevisionPlugin.commithash())
+            args[0]['process.env']['VUE_APP_VERSION'] = JSON.stringify(gitRevisionPlugin.version())
+            args[0]['process.env']['VUE_APP_BRANCH'] = JSON.stringify(gitRevisionPlugin.branch())
+            // args[0]['process.env']['VUE_APP_LASTCOMMITDATETIME'] = JSON.stringify(gitRevisionPlugin.lastcommitdatetime())
+            return args
+        })
     },
     devServer: {
         disableHostCheck: true
