6181e2b1b5c02eeb7cf04c83c87550d3d3b2200d
Gautier <gautier@nexus.ethz.ch>
2022-03-08 12:35:27 +0100

    improved translation process + translated more files

--- svip-o-vue/src/i18n/translate.py
+++ svip-o-vue/src/i18n/translate.py
@@ -1,14 +1,13 @@
-from django.core.management.base import BaseCommand
-
 import os, re
 from bs4 import BeautifulSoup
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+I18N_DIR = os.path.join(BASE_DIR, 'i18n')
+LOCALES_DIR = os.path.join(BASE_DIR, 'locales')
 
 def add_to_json(newStr):
-  json_doc = open(f"{json_path}/en.json", "r+")
+  json_doc = open(f"{LOCALES_DIR}/en.json", "r+")
   json_lines = json_doc.readlines()
   for line in json_lines:
     substrings = line.split('"')
@@ -39,7 +38,7 @@ def substrings_outside_of_curly_braces(string):
   return strings_of_interest
 
 
-def replace_in_file(html_lines, element, original_string):
+def replace_in_file(path_of_file_to_translate, html_lines, element, original_string):
   new_string = '{{ $t("%s")}}' % (original_string)
   for idx, line in enumerate(html_lines):
     patterns_in_line = len(re.findall(original_string, line))
@@ -50,12 +49,11 @@ def replace_in_file(html_lines, element, original_string):
       new_line = original_line.replace(original_string, new_string, i+1).replace(new_string, original_string, i)
       temp_lines[idx] = new_line
 
-      curation_path = os.path.join(BASE_DIR, 'translation')
-      temp_doc = open(f"{curation_path}/temp.html", "r+")
+      temp_doc = open(f"{I18N_DIR}/temp.html", "r+")
       temp_doc.write("".join(temp_lines))
       temp_doc.close()
       
-      temp_doc = open(f"{curation_path}/temp.html", "r+")
+      temp_doc = open(f"{I18N_DIR}/temp.html", "r+")
       temp_soup = BeautifulSoup(temp_doc, 'html.parser')
       temp_text = temp_soup.find_all()[0].text
       temp_doc.close()
@@ -64,7 +62,7 @@ def replace_in_file(html_lines, element, original_string):
         # the wrong segment has been replaced because the innerHTML text is the same. Try replacing next pattern
         pass
       else:
-        html_doc = open(f"{curation_path}/html_content.html", "r+")
+        html_doc = open(path_of_file_to_translate, "r+")
         html_doc.write("".join(temp_lines))
         html_doc.close()
         print('Text is replaced.')
@@ -72,7 +70,7 @@ def replace_in_file(html_lines, element, original_string):
   print("The string was not found. This is unexpected.")
 
 
-def get_strings(soup, html_lines):
+def get_strings(path_of_file_to_translate, soup, html_lines):
   templateElement = soup.find_all()[0]
   innerHTML = templateElement.text
   # check presence of letters in substring:
@@ -99,28 +97,26 @@ def get_strings(soup, html_lines):
               break
             elif acceptStr in ["Y", "y"]:
               add_to_json(str_to_replace)
-              replace_in_file(html_lines, templateElement, str_to_replace)
+              replace_in_file(path_of_file_to_translate, html_lines, templateElement, str_to_replace)
             else:
               print("Wrong command.")
-          print('\n')
+  print('\n')
 
 
-class Command(BaseCommand):
-  help = """
-    Replace strings that need to be translated in a file for which the user provides the path.
-    Adds each translated segment to the JSON file for English content in the "locales" directory.
-  """
+def get_file_path():
+  
+  gross_path = input("Enter the path of file to translate :\n")
+  path_of_file_to_translate = os.path.join(BASE_DIR, gross_path.split("src/")[1])
+  
+  html_doc = open(path_of_file_to_translate, "r+")
+  soup = BeautifulSoup(html_doc, 'html.parser')
+  html_doc.close()
 
-  def handle(self, *args, **options):
-    curation_path = os.path.join(BASE_DIR, 'translation')
-    json_path = os.path.join(BASE_DIR, '../svip-o-vue/src/locales')
+  html_doc = open(path_of_file_to_translate, "r+")
+  html_lines = html_doc.readlines()
+  html_doc.close()
 
-    html_doc = open(f"{curation_path}/html_content.html", "r+")
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    html_doc.close()
+  get_strings(path_of_file_to_translate, soup, html_lines)
 
-    html_doc = open(f"{curation_path}/html_content.html", "r+")
-    html_lines = html_doc.readlines()
-    html_doc.close()
 
-    get_strings(soup, html_lines)
+get_file_path()
