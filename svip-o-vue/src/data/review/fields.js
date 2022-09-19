f91a87280478d0f0bc1dd382658f092914154de1
aesche <anastasia.escher@gmail.com>
2022-09-19 11:19:15 +0200

    Add status column to review table

--- svip-o-vue/src/data/review/fields.js
+++ svip-o-vue/src/data/review/fields.js
@@ -1,47 +1,47 @@
 import {abbreviatedName} from '@/utils';
 
 export default [
-	{
-		key: 'gene',
-		label: 'Gene',
-		sortable: true,
-	},
-	{
-		key: 'variant',
-		label: 'Variant',
-		sortable: true,
-	},
-	{
-		key: 'hgvs',
-		label: 'HGVS.c',
-		sortable: false,
-	},
-	{
-		key: 'diseases',
-		label: 'Disease(s)',
-		sortable: true,
-	},
-	{
-		key: 'status',
-		label: 'Status',
-		sortable: true,
-	},
+    {
+        key: 'gene',
+        label: 'Gene',
+        sortable: true,
+    },
+    {
+        key: 'variant',
+        label: 'Variant',
+        sortable: true,
+    },
+    {
+        key: 'hgvs',
+        label: 'HGVS.c',
+        sortable: false,
+    },
+    {
+        key: 'diseases',
+        label: 'Disease(s)',
+        sortable: true,
+    },
+    {
+        key: 'status',
+        label: 'Reviews',
+        sortable: true,
+    },
 
-	{
-		key: 'variant_status',
-		label: 'variant_status',
-		sortable: true,
-	},
+    {
+        key: 'variant_status',
+        label: 'Status',
+        sortable: true,
+    },
 
-	{
-		key: 'curators',
-		label: 'Curator(s)',
-		sortable: true,
-		filterByFormatted: x => x.map(z => abbreviatedName(z.name).abbrev).join(', '),
-	},
-	{
-		key: 'action',
-		label: 'Action',
-		sortable: false,
-	},
+    {
+        key: 'curators',
+        label: 'Curator(s)',
+        sortable: true,
+        filterByFormatted: x => x.map(z => abbreviatedName(z.name).abbrev).join(', '),
+    },
+    {
+        key: 'action',
+        label: 'Action',
+        sortable: false,
+    },
 ];
