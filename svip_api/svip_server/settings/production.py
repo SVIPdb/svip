0fab24a968e57770ba7003037d3d4ab27d375fe5
dameyerdave <dameyerdave@gmail.com>
2022-11-16 10:05:27 +0100

    [FIX] production config should set DEBUG to False

--- svip_api/svip_server/settings/production.py
+++ svip_api/svip_server/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
 # put in some admins who should receive emails if things go wrong
 if 'DJANGO_SU_NAME' in os.environ and 'DJANGO_SU_EMAIL' in os.environ:
