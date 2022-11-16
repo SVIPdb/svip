c768520c3bb00a5b327911fb5d4b1f580b38e6eb
dameyerdave <dameyerdave@gmail.com>
2022-11-16 10:06:27 +0100

    [CLN] Removed files not used anymore

--- dc_private.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker-compose -f docker-compose.yml -f compose_configs/prod.yml -f compose_configs/sites/sib-svip-private.yml "$@"
-
--- deployment/run_registry.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d -p 15000:5000 --restart=always --name registry registry:2
