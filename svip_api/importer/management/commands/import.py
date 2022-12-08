64ffef89eea92c279b79c10df6654dc60c826d73
dameyerdave <dameyerdave@gmail.com>
2022-12-08 16:20:31 +0100

    [FEAT] Prepared the importer management command

---
+++ svip_api/importer/management/commands/import.py
@@ -0,0 +1,10 @@
+# The import command is able to import
+# - a single variant given by console arguments <-- postpone
+# - multiple variants given by a file (vcf, tsv or csv) <-- postpone
+# - handle the svip_submittedvatiant table <-- TODO
+
+# 1) query VEP (vep_offline_rest) <-- TODO
+# 2) query evidences from available sources (civic, ...) <-- postpone
+# 3) add objects to db <-- TODO
+
+# What about structured variants <-- postpone
